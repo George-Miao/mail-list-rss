4bc50c2b3cc1161547a7162b5d7e2b48be0176f4
George Miao <gm@miao.dev>
2021-12-15 22:43:37 +0800

    chore: change default domain to example.com so it's legit

--- src/config.rs
+++ src/config.rs
@@ -20,7 +20,7 @@ impl Config {
             web_port: var("WEB_PORT").map_or_else(|_| Ok(8080), |x| x.parse())?,
             smtp_port: var("SMTP_PORT").map_or_else(|_| Ok(10000), |x| x.parse())?,
             per_page: var("PER_PAGE").map_or_else(|_| Ok(10), |x| x.parse())?,
-            domain: var("DOMAIN").unwrap_or_else(|_| "localhost".to_owned()),
+            domain: var("DOMAIN").unwrap_or_else(|_| "example.com".to_owned()),
             mongo_con_str: var("MONGO_CON_STR")
                 .unwrap_or_else(|_| "mongodb://localhost:27017".to_owned()),
             mongo_db_name: var("MONGO_DB_NAME").unwrap_or_else(|_| "mail-list-rss".to_owned()),
