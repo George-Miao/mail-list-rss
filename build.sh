f6fd0e364f5b70a1996437b6d1cd63ad48f3db21
George Miao <gm@miao.dev>
2022-01-16 01:30:06 -0500

    fix: failed to build in docker

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-pushd front
-yarn build
-popd
+command -v apt && \
+ apt update  && \
+ apt upgrade && \
+ apt install curl && \
+ curl -fsSL https://deb.nodesource.com/setup_current.x | bash -
+cd front
+npm run build
+cd ..
 cargo build --release
