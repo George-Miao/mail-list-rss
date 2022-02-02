7d9dcdeb9d925ed502a1dc940395a693eb7298f7
George Miao <gm@miao.dev>
2022-02-02 12:17:49 -0500

    fix: build script

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-command -v apt && \
- apt-get update  && \
- apt-get upgrade && \
- apt-get install curl gcc g++ make  && \
+command -v apt-get update -y && \
+ apt-get upgrade -y && \
+ apt-get install -y curl gcc g++ make  && \
  curl -fsSL https://deb.nodesource.com/setup_current.x | bash - && \
  apt-get install -y nodejs
 cd front
